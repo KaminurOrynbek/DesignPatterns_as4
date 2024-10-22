baed59571e5509a1f161648653e03611b69f6336
KaminurOrynbek <kaminurorinbek@gmail.com>
2024-10-22 17:49:22 +0500

    4

---
+++ src/main/java/Mediator/PremiumUser.java
@@ -0,0 +1,19 @@
+package Mediator;
+
+public class PremiumUser extends User {
+
+    public PremiumUser(ChatMediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    @Override
+    public void send(String message) {
+        System.out.println(this.name + " [Premium] sends: " + message);
+        mediator.sendMessage(message, this);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println(this.name + " [Premium] receives: " + message);
+    }
+}
